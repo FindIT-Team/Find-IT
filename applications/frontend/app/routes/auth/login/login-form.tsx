ba832c3a78af5963855c14d9dfd8c6ac17cf8042
Aspis <vladimir.lebedev.07@mail.ru>
2024-02-20 20:47:57 +0300

    (Frontend) Changed the colors of the login page and translated it into Russian

--- applications/frontend/app/routes/auth/login/login-form.tsx
+++ applications/frontend/app/routes/auth/login/login-form.tsx
@@ -1,87 +1,59 @@
-import { Field, Formik } from 'formik';
 import {
   Box,
   Button,
   Checkbox,
   Flex,
   FormControl,
-  FormErrorMessage,
   FormLabel,
   Input,
   VStack,
 } from '@chakra-ui/react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Schema, schema } from '~/routes/auth/login/shema';
+import { useRemixForm } from 'remix-hook-form';
+import { Form as RemixForm } from '@remix-run/react';
 
-export default function App() {
+export function Form() {
+  const remixForm = useRemixForm<Schema>({
+    mode: 'all',
+    resolver: zodResolver(schema),
+  });
   return (
     <Flex bg="gray.100" align="center" justify="center" h="100vh">
       <Box bg="white" p={6} rounded="md" w={64}>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-            rememberMe: false,
-          }}
-          onSubmit={(values) => {
-            alert(JSON.stringify(values, null, 2));
-          }}
-        >
-          {({ handleSubmit, errors, touched }) => (
-            <form onSubmit={handleSubmit}>
-              <VStack spacing={4} align="flex-start">
-                <FormControl>
-                  <FormLabel htmlFor="email">Почта</FormLabel>
-                  <Field
-                    as={Input}
-                    id="email"
-                    name="email"
-                    type="email"
-                    variant="filled"
-                    background={'gray.200'}
-                  />
-                </FormControl>
-                <FormControl isInvalid={!!errors.password && touched.password}>
-                  <FormLabel htmlFor="password">Пароль</FormLabel>
-                  <Field
-                    as={Input}
-                    id="password"
-                    name="password"
-                    type="password"
-                    background={'gray.200'}
-                    variant="filled"
-                    validate={(value: string) => {
-                      let error;
-
-                      if (value.length < 6) {
-                        error =
-                          'Пароль должен содержать как минимум 6 символов';
-                      }
-
-                      return error;
-                    }}
-                  />
-                  <FormErrorMessage>{errors.password}</FormErrorMessage>
-                </FormControl>
-                <Field
-                  as={Checkbox}
-                  id="rememberMe"
-                  name="rememberMe"
-                  color={'black'}
-                >
-                  Запомнить меня?
-                </Field>
-                <Button
-                  type="submit"
-                  background={'indigo.600'}
-                  color={'white'}
-                  _hover={{ background: 'indigo.700' }}
-                  width="full"
-                >
-                  Войти
-                </Button>
-              </VStack>
-            </form>
-          )}
-        </Formik>
+        <RemixForm onSubmit={remixForm.handleSubmit}>
+          <VStack spacing={4} align="flex-start">
+            <FormControl>
+              <FormLabel htmlFor="email">Почта</FormLabel>
+              <Input
+                {...remixForm.register('uniq')}
+                variant="filled"
+                background={'gray.200'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Пароль</FormLabel>
+              <Input
+                {...remixForm.register('password')}
+                type="password"
+                background={'gray.200'}
+                variant="filled"
+              />
+            </FormControl>
+            <Checkbox id="rememberMe" name="rememberMe" color={'black'}>
+              Запомнить меня?
+            </Checkbox>
+            <Button
+              type="submit"
+              background={'indigo.600'}
+              color={'white'}
+              _hover={{ background: 'indigo.700' }}
+              width="full"
+            >
+              Войти
+            </Button>
+          </VStack>
+        </RemixForm>
       </Box>
     </Flex>
   );
--- applications/frontend/app/routes/auth/login/route.tsx
+++ applications/frontend/app/routes/auth/login/route.tsx
@@ -1,9 +1,27 @@
 import { Link } from '~/components/link';
 // import LoginWith from '~/routes/auth/login/login-with';
-import LoginForm from '~/routes/auth/login/login-form';
+import { Form } from '~/routes/auth/login/login-form';
 import { loginUrls } from '~/routes/auth/login/login-urls';
+import { redirect } from '@remix-run/react';
+import { ActionFunctionArgs } from '@remix-run/node';
+import { getValidatedFormData } from 'remix-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { schema } from '~/routes/auth/login/shema';
 
-const LoginScreen = () => {
+export async function action({ request }: ActionFunctionArgs) {
+  const data = (await getValidatedFormData(request, zodResolver(schema))).data;
+  const res = await fetch(`http://api.${process.env.DOMAIN}/auth/login`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return redirect(`http://api.${process.env.DOMAIN}/auth`);
+}
+
+export default function Page() {
   return (
     <>
       <div>
@@ -17,11 +35,10 @@ const LoginScreen = () => {
           <div>
             <hr />
           </div>
-          <LoginForm />
+          <Form />
         </div>
         <div />
       </div>
     </>
   );
-};
-export default LoginScreen;
+}
