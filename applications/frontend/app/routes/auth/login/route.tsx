b59fbe288cfdabbb16bcd8da9ce7ccc4ca5e8a9a
Даня Сафин <danyasafin@icloud.com>
2024-02-22 10:19:34 +0300

    (Frontend) Refactor

--- applications/frontend/app/routes/auth/login/route.tsx
+++ applications/frontend/app/routes/auth/login/route.tsx
@@ -15,14 +15,12 @@ import {
   Center,
   chakra,
   Divider,
-  Heading,
   HStack,
-  Image,
   VStack,
 } from '@chakra-ui/react';
 import { UniqField } from '~/routes/auth/login/fields/uniq-field';
 import { PasswordField } from '~/routes/auth/login/fields/password-field';
-import { externalAuth } from '~/routes/auth/login/url';
+import { ExternalAuth } from '~/routes/auth/login/external-auth';
 
 export async function action({ request }: ActionFunctionArgs) {
   const data = (await getValidatedFormData(request, zodResolver(schema))).data;
@@ -60,25 +58,15 @@ export default function Page() {
             wrap={'nowrap'}
           >
             <VStack padding={12} spacing={12} flexShrink={0}>
-              <VStack>
-                <Heading fontSize={'2xl'}>Войти с помощью</Heading>
-                <HStack>
-                  {externalAuth.map(({ id, url, img }) => (
-                    <Button key={id} p={2}>
-                      <Link to={url} w={8} h={8}>
-                        <Image src={img} alt={id} w={'full'} h={'full'} />
-                      </Link>
-                    </Button>
-                  ))}
-                </HStack>
-              </VStack>
-              <Center width={'full'}>
+              <ExternalAuth />
+              <Center width={'full'} opacity={'60%'}>
                 <Divider />
                 <chakra.span
                   position={'absolute'}
                   px={2}
                   background={'white'}
-                  fontSize={'lg'}
+                  fontSize={'md'}
+                  fontWeight={'semibold'}
                 >
                   или
                 </chakra.span>
@@ -101,15 +89,6 @@ export default function Page() {
                 </HStack>
               </VStack>
             </VStack>
-            <Image
-              src={'/login-side-image.jpg'}
-              display={'block'}
-              objectFit={'cover'}
-              aspectRatio={'1/2'}
-              objectPosition={'center center'}
-              maxWidth={'400px'}
-              height={'40%'}
-            />
           </HStack>
         </Form>
       </RemixFormProvider>
