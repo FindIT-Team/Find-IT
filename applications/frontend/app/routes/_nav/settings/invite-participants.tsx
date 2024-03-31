b8110ec99de7aac833ef2e204d893da2ccf2db0d
Gromov Makar <gromovmakar3@gmail.com>
2024-03-31 22:01:24 +0300

    Test

---
+++ applications/frontend/app/routes/_nav/settings/invite-participants.tsx
@@ -0,0 +1,34 @@
+import { Button, Center, useToast } from '@chakra-ui/react';
+import { useLoaderData } from '@remix-run/react';
+import { loader } from '~/routes/_nav/settings/profile/route';
+import { useEffect, useState } from 'react';
+
+export function InviteParticipants() {
+  const [clipboard, setClipboard] = useState<Clipboard | null>(null);
+  useEffect(() => setClipboard(navigator.clipboard), []);
+  const toast = useToast();
+  const { url } = useLoaderData<typeof loader>();
+  return (
+    <Center
+      justifyContent={'end'}
+      padding={3}
+      alignItems={'end'}
+      boxSize={'full'}
+    >
+      <Button
+        onClick={() => {
+          clipboard?.writeText(url);
+          toast({
+            title: 'Ссылка есть.',
+            status: 'success',
+            duration: 3000,
+          });
+        }}
+        colorScheme="gray"
+        variant="outline"
+      >
+        Пригласить участника
+      </Button>
+    </Center>
+  );
+}
--- applications/frontend/app/routes/_nav/settings/profile/route.tsx
+++ applications/frontend/app/routes/_nav/settings/profile/route.tsx
@@ -1,13 +1,19 @@
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { InviteParticipants } from '~/routes/_nav/settings/invite-participants';
+import { json } from '@remix-run/react';
 
 export const meta: MetaFunction = () => [
   { title: 'Настройки профиля | FindIT' },
 ];
 
-export async function loader({}: LoaderFunctionArgs) {
-  return null;
+export async function loader({ request }: LoaderFunctionArgs) {
+  return json({ url: request.url });
 }
 
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <InviteParticipants />
+    </>
+  );
 }
